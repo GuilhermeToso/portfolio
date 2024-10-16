31f6c471b0809fc87eb6fb7050b3b7044ba2ac9f
Guilherme Toso <gui.m.toso@gmail.com>
2024-10-16 17:09:47 -0300

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,45 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import { Analytics } from '@vercel/analytics/react';
 import Providers from "@/app/providers";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Guilherme M. Toso',
-    description: 'Software | AI Engineer',
-    icons: {
-        icon: '/icon.png',
-    },
-    openGraph: {
-        title: 'Guilherme M. Toso',
-        description: 'Software | AI Engineer',
-        url: 'https://guilhermetoso.dev',
-        siteName: 'guihermetoso.dev',
-        images: [
-            {
-                url: '/guilherme-toso.png',
-                width: 600,
-                height: 666,
-            },
-        ],
-        locale: 'pt_BR',
-        type: 'website',
-    },
+  title: "Guilherme M. Toso",
+  description: "Software | AI Engineer",
+  icons: {
+    icon: "/icon.png",
+  },
+  openGraph: {
+    title: "Guilherme M. Toso",
+    description: "Software | AI Engineer",
+    url: "https://guilhermetoso.dev",
+    siteName: "guihermetoso.dev",
+    images: [
+      {
+        url: "/guilherme-toso.png",
+        width: 600,
+        height: 666,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
-
 export default async function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
-
-    return (
-      <html lang="en">
+  return (
+    <html lang="en">
       <body className={`${inter.className} overflow-y-scroll`}>
-      <Providers>
-          {children}
-      </Providers>
-      <Analytics></Analytics>
+        <Providers>{children}</Providers>
       </body>
-      </html>
+      <GoogleTagManager gtmId="G-8C6EFPM7L8" />
+    </html>
   );
 }
